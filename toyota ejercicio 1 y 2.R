b5d9b864b738db40fcd8818b643566ee2336cc00
Nbmv345 <105770674+Nbmv345@users.noreply.github.com>
2022-05-24 18:59:45 -0500

    Update toyota ejercicio 1 y 2.R

--- toyota ejercicio 1 y 2.R
+++ toyota ejercicio 1 y 2.R
@@ -1,3 +1,53 @@
+
+#EJERCICIO 1
+car.df <- read.csv("ToyotaCorolla.csv")
+car.df <- car.df[1:1000, ]
+selected.var <- c(3, 4, 7, 8, 9, 10, 12, 13, 14, 17, 18)
+View(car.df)
+
+set.seed(1) # set seed for reproducing the partition
+train.index <- sample(c(1:1000), 600)
+train.df <- car.df[train.index, selected.var]
+valid.df <- car.df[-train.index, selected.var]
+car.lm <- lm(Price ~ ., data = train.df)
+options(scipen = 999)
+
+summary(car.lm)
+
+library(forecast)
+
+car.lm.pred <- predict(car.lm, valid.df)
+options(scipen=999, digits = 0)
+some.residuals <- valid.df$Price[1:20] - car.lm.pred[1:20]
+data.frame("Predicted" = car.lm.pred[1:20], "Actual" = valid.df$Price[1:20],
+"Residual" = some.residuals)
+
+
+car.lm.pred <- predict(car.lm, valid.df)
+all.residuals <- valid.df$Price - car.lm.pred
+length(all.residuals[which(all.residuals > -1406 & all.residuals < 1000)])/400
+
+
+set.seed(123)
+train.rows <- sample(rownames(car.df), dim(car.df)[1]*0.5)
+set.seed(123)
+valid.rows <- sample(setdiff(rownames(car.df), train.rows),dim(car.df)[1]*0.3)
+set.seed(123)
+test.rows <- setdiff(rownames(car.df), union(train.rows, valid.rows))
+
+train.data <- car.df[train.rows, ]
+valid.data <- car.df[valid.rows, ]
+test.data <- car.df[test.rows, ]
+
+
+car.df <- lm(Price ~ ., data = test.data)
+summary(car.df)
+
+#---------------------------------------------------------------------------------
+
+#EJERCICIO 2
+
+
 library(dummies)
 library(GGally)
 library(gplots)
